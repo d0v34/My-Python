833483fcb255f3be9cb667aa4dd200c07d425af5
d0v34 <fiat126p@gmail.com>
2019-10-01 10:27:41 +0200

    New contenet

--- test1.py
+++ test1.py
@@ -1,2 +1,9 @@
 print("Hej")
-
+print("jak masz na imie?")
+name=input()
+print("Witaj", name)
+print("Ile masz lat?"
+age=input(int())
+print("Znasz peron Harrego Pottera?")
+platform=input(int())
+print("czy jest majac lat", age, "jechac z peronu", platform, "do Hogwartu?")
