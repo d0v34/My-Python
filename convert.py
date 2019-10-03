9be23cb8fa3cbc6368cdb1fc86b243915291d8bc
d0v34 <fiat126p@gmail.com>
2019-10-03 10:51:49 +0200

    New ver.

--- convert.py
+++ convert.py
@@ -1,4 +1,6 @@
-print("Podaj watosc w cm")
-i = input()
+#print("Podaj watosc w cm")
+#i = input()
+# To mozna w jednej lini
+i = int(input("poedaj dlugosc w cm: "))
 i = i / 100
-print("Wartosc w metrach wynosi %6.2f m" % (i))
+print("Wartosc w metrach wynosi %r m" % (i))
