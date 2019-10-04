e7b3b82ff3b120a7fa257d6bb526e21c6f9a16e2
d0v34 <fiat126p@gmail.com>
2019-10-04 09:47:43 +0200

    Update

--- napisy.py
+++ napisy.py
@@ -1,4 +1,4 @@
 sentence = input("Wpisz dowolne zdanie: ")
 print("="*10)
 print("Twoje zdanie zawiera ", len(sentence), " znakow")
-print("Drugi wyraz to: ", split(sentence)[2])
+print("Drugi wyraz to: ", sentence.split()[2])
