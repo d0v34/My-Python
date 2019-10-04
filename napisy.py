c3703fa4ef538fddae6489c7ad2bf6d7dffa35dc
d0v34 <fiat126p@gmail.com>
2019-10-04 09:56:58 +0200

    Update

--- napisy.py
+++ napisy.py
@@ -4,3 +4,7 @@ print("Twoje zdanie zawiera ", len(sentence), " znakow")
 #Numeracja idzie od zera
 print("Drugi wyraz to: ", sentence.split()[1])
 print("Siodma litera to: \"", sentence[6], "\"")
+print("Siodma litera to: \"", sentence[6], "\"")
+print("Czwarta litera od konca to: \"", sentence[-4], "\"")
+print("37 litera to: \"", sentence[37], "\"")
+
