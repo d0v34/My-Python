bf94186401291a370319a3cfe4a98fc94e0d1fea
d0v34 <fiat126p@gmail.com>
2019-10-04 10:02:15 +0200

    Finish update

--- napisy.py
+++ napisy.py
@@ -1,10 +1,11 @@
+# Kurs Pythona jest prosty i przyjemny.
 sentence = input("Wpisz dowolne zdanie: ")
 print("="*10)
 print("Twoje zdanie zawiera ", len(sentence), " znakow")
-#Numeracja idzie od zera
+# Numeracja idzie od zera
 print("Drugi wyraz to: ", sentence.split()[1])
-print("Siodma litera to: \"", sentence[6], "\"")
-print("Dwunasta litera to: \"", sentence[11], "\"")
-print("Czwarta litera od konca to: \"", sentence[-4], "\"")
-print("37 litera to: \"", sentence[36], "\"")
+print("Siodmy znak to: \"", sentence[6], "\"")
+print("Dwunasty znak to: \"", sentence[11], "\"")
+print("Czwarty znak od konca to: \"", sentence[-4], "\"")
+print("37 znak to: \"", sentence[36], "\"")
 
