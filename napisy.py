799794f7356e7e916ad1c0da53e01e4575520f88
d0v34 <fiat126p@gmail.com>
2019-10-04 10:10:40 +0200

    The last final update

--- napisy.py
+++ napisy.py
@@ -1,11 +1,12 @@
 # Dziala dobrze dla "Kurs Pythona jest prosty i przyjemny."
 sentence = input("Wpisz dowolne zdanie: ")
-print("="*10)
-print("Twoje zdanie zawiera ", len(sentence), " znakow")
+print("="*30)
+print("Twoje zdanie zawiera", len(sentence), "znakow")
 # Numeracja idzie od zera
 print("Drugi wyraz to: ", sentence.split()[1])
 print("Siodmy znak to: \"", sentence[6], "\"")
 print("Dwunasty znak to: \"", sentence[11], "\"")
 print("Czwarty znak od konca to: \"", sentence[-4], "\"")
 print("37 znak to: \"", sentence[36], "\"")
-
+print("="*30)
+print(sentence)
