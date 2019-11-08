85d7bf543fffc767c4edcdebe21076fbb8aa3f43
d0v34 <fiat126p@gmail.com>
2019-11-08 13:49:35 +0100

    update

--- adult.py
+++ adult.py
@@ -1,7 +1,10 @@
 print("Ile masz lat?")
 age = int(input())
 ##tofull = 18 - age
-if (age >= 18):
+elif (age >= 18):
     print("Jests pelnoletni")
 else:
     print("Zostalo ci", 18-age ,"lat do pelnoletnosci")
+if (age > 100):
+    print ("To naprawde twoj wiek?")
+
