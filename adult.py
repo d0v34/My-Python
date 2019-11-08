5815787e634ef2d9805f291f82d36c7950033d16
d0v34 <fiat126p@gmail.com>
2019-11-08 13:55:04 +0100

    Update elif

--- adult.py
+++ adult.py
@@ -1,9 +1,9 @@
 print("Ile masz lat?")
 age = int(input())
 ##tofull = 18 - age
-if (age >= 18):
-    print("Jests pelnoletni")
-elif (age > 100):
+if age >= 18:
+    print("Jestes pelnoletni")
+elif age > 100:
     print ("To naprawde twoj wiek?")
 else:
     print("Zostalo ci", 18-age ,"lat do pelnoletnosci")
