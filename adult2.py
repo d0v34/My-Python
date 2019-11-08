15fb41f93d02a7cbd0625a4a579c29a965cc5585
d0v34 <fiat126p@gmail.com>
2019-11-08 13:57:59 +0100

    Update ifif

---
+++ adult2.py
@@ -0,0 +1,9 @@
+print("Ile masz lat?")
+age = int(input())
+##tofull = 18 - age
+if age >= 18 :
+    print("Jests pelnoletni")
+if age > 100 :
+    print("To naprawdêwóiek?")
+else :
+    print("Zostalo ci", 18-age ,"lat do pelnoletnosci")
