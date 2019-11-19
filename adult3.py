2e93e88669f35564ca6fdbf90aa4309ccfe86471
d0v34 <fiat126p@gmail.com>
2019-11-19 12:00:26 +0100

    New file

---
+++ adult3.py
@@ -0,0 +1,10 @@
+print("Ile masz lat?")
+age = int(input())
+
+if age < 18 :
+    print("Jestes pelnoletni")
+    print("Zapraszam za", 18-age ,"lat")
+if age >= 18 :
+    print("Uzytkownik pelnoletni")
+if age > 100 :
+    print("Barwo! Zycze Ci 200 lat!")
