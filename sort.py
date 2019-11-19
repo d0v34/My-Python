bce7011d7ba4a85bf1b45cea89d570e4ded050a6
d0v34 <fiat126p@gmail.com>
2019-11-19 13:44:49 +0100

    Uder construction...

---
+++ sort.py
@@ -0,0 +1,23 @@
+print("Podaj 1 liczbe")
+a = int(input())
+print("Podaj 2 liczbe")
+b = int(input())
+print("Podaj 3 liczbe")
+c = int(input())
+if a < b :
+    elif a <c
+
+if b < a and b < c
+    srednia = b
+    najmnieszaj = c
+if a > b and a < c :
+    srednia = a
+elif
+    najwieksza = b
+    najmnieszaj = c
+else
+    najmnieszaj = a
+    srednia = 
+    najwieksza =
+    
+    
